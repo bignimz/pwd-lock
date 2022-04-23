dec06ec3132fe864384e318acffa218d877a6a23
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-04-23 23:53:51 +0300

    Created a credentials list

---
+++ credentials.py
@@ -0,0 +1,17 @@
+import User
+
+
+class Credentials:
+    """"
+    Create credentials class that generates credentials object blueprint
+    """
+
+    credentials_list = []
+
+    @classmethod
+    def verify_user(cls, username, password):
+        """
+        Method to verify if user is available or not in the users list
+        """
+        user = ""
+        for user in User.user_list:

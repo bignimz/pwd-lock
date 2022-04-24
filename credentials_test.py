5f7079558b4dcdc8d1ab87d67cd71c53bfa09db0
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-04-24 18:30:55 +0300

    created a test init method to check if new credentials is initialized

--- credentials_test.py
+++ credentials_test.py
@@ -17,6 +17,14 @@ class TestCredentials(unittest.TestCase):
         self.new_credential = Credentials(
             'Linkedin', 'nimrod-musungu', 'Lkdn@123')
 
+    def test_init(self):
+        """
+        Test case to check if a new Credentials instance has been initialized correctly
+        """
+        self.assertEqual(self.new_credential.account, 'Linkein')
+        self.assertEqual(self.new_credential.userName, 'nimrod-musungu')
+        self.assertEqual(self.new_credential.password, 'Lkdn@123')
+
 
 if __name__ == "__main__":
     unittest.main()

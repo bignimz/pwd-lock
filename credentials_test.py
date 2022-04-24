cba3f962554186e94d2bc54bd3d63851c3fc5dc7
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-04-24 18:29:02 +0300

    created a setup method to test for defaults account credentials

--- credentials_test.py
+++ credentials_test.py
@@ -1,3 +1,22 @@
 import unittest
 from user import User
 from credentials import Credentials
+
+
+class TestCredentials(unittest.TestCase):
+    """
+    A test class that defines test cases for credentials class
+
+    """
+
+    def setUp(self):
+        """
+        Method that runs before each individual credentials test methods run.
+
+        """
+        self.new_credential = Credentials(
+            'Linkedin', 'nimrod-musungu', 'Lkdn@123')
+
+
+if __name__ == "__main__":
+    unittest.main()
